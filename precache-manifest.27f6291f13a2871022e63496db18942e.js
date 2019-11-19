69ad450a5a7397dc808c0d87ad11db9c97e6b46a
xandyk <sandy@android-b62c5a1a4afea753.localdomain>
2019-11-18 16:45:14 -0800

    Updates

---
+++ precache-manifest.27f6291f13a2871022e63496db18942e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cffc7e6bf4239de9ced32ec067098e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "746c8c41c34fbf15eab8",
+    "url": "/monsters-rolodex/static/css/main.c366cad3.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "746c8c41c34fbf15eab8",
+    "url": "/monsters-rolodex/static/js/main.84190948.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
